0fd626b8d251e4bf063b768e8da030a67871e74f
Jordan Zhou <38576009+jordanzhou319@users.noreply.github.com>
2018-06-01 11:57:40 -0400

    Update intron_splicer.py

--- intron_splicer.py
+++ intron_splicer.py
@@ -11,6 +11,8 @@ mantooth = ''.join(i for i in whore_island if not i.isdigit())
 
 penetration = []
 printers = []
+hookers = []
+porn = []
 
 for blue_waffle in range(0,va):
     blue = int(input('Beginning of intron ' + str(na) + ': '))
@@ -23,11 +25,20 @@ print('\n')
 
 if va == 1:
     trump = mantooth[:penetration[0]] + mantooth[printers[0]:]
+    hookers.append(len(trump))
     print('Exon: ' + str(trump))
     print('Exon Length: ' + str(len(trump)) + 'bp')
+    print('\n')
+    
+    herpes = sum(hookers)
+    chlamydia = (herpes/len(gi))
+    print('Total coding length: ' + str(herpes) + 'bp')
+    print('Percent coding: ' + str('{:.2%}'.format(chlamydia)))
 
 else:
     penis = mantooth[:penetration[0]]
+    hookers.append(len(penis))
+    porn.append(penis)
     print('Exon 1: ' + str(penis))
     print('Exon Length: ' + str(len(penis)) + 'bp')
     print('\n')
@@ -40,7 +51,19 @@ else:
         fucking += 1
         stupid += 1
         pirate_hooker += 1
+        hookers.append(len(shit))
+        porn.append(shit)
         
     fuck = mantooth[printers[-1]:]
+    hookers.append(len(fuck))
+    porn.append(fuck)
     print('Exon ' + str(pirate_hooker + 1) + ': ' + str(fuck))
-    print('Exon Length: ' + str(len(fuck)) + 'bp')    
+    print('Exon Length: ' + str(len(fuck)) + 'bp')  
+    print('\n')
+    
+    herpes = sum(hookers)
+    chlamydia = (herpes/len(gi)) 
+    print('Complete coding region:')
+    print(*porn,sep='')
+    print('Total coding length: ' + str(herpes) + 'bp')
+    print('Percent coding: ' + str('{:.2%}'.format(chlamydia)))
